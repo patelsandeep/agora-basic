f1870c30c5d0d97b4dc47f2316ddfe7e44ae28f2
MehulRajBYPTech <53267243+MehulRajBYPTech@users.noreply.github.com>
2022-12-21 18:35:25 +0530

    agora live interaction with web

--- agora_demo/lib/utils/constants.dart
+++ agora_demo/lib/utils/constants.dart
@@ -1,10 +1,11 @@
-const String appId = "XXXXXXXXXXXX";
-const String channelName = "XXXX";
+const String appId = "6e83ccd477b441cc9b92256566ba9a2b";
+const String channelName = "video-call";
 // live interaction token
 // const String token = "XXXXXXXXXXXX";
 
 // video/audio call token
-const String token = "XXXXXXXXXXXXXX";
+const String token =
+    "007eJxTYDganlztxDLTmcHm3qPlU5/MWPfB9EfX7IU1D67NOH84+PgNBQazVAvj5OQUE3PzJBMTw+RkyyRLIyNTM1Mzs6REy0SjJF3mxckNgYwMeo8PMDBCIYjPxVCWmZKar5ucmJPDwAAA8kokUQ==";
 
 const String chatUserToken =
     "007eJxTYMjcY2i48uHZzgXpEsfmKx2auyGykqnp4s3yJ9pnFRxvyxoqMJilWhgnJ6eYmJsnmZgYJidbJlkaGZmamZqZJSVaJholcXxZlNwQyMgwJeMnAyMDKxAzMoD4KgwmBokWZiamBrrmaWbmuoaGqSm6FkmmaboGSYYWiclmicbGyWYAuhsoBw==";
