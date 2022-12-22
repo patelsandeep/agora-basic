01f20a491a05d442adfb8667280c312d37a7c959
Sandeep Patel <patelsandeep1987@gmail.com>
2022-12-22 12:44:06 +0530

    Update constants.dart

--- agora_demo/lib/utils/constants.dart
+++ agora_demo/lib/utils/constants.dart
@@ -1,11 +1,11 @@
 const String appId = "6e83ccd477b441cc9b92256566ba9a2b";
-const String channelName = "video-call";
+const String channelName = "video-call-2";
 // live interaction token
 // const String token = "XXXXXXXXXXXX";
 
 // video/audio call token
 const String token =
-    "007eJxTYDganlztxDLTmcHm3qPlU5/MWPfB9EfX7IU1D67NOH84+PgNBQazVAvj5OQUE3PzJBMTw+RkyyRLIyNTM1Mzs6REy0SjJF3mxckNgYwMeo8PMDBCIYjPxVCWmZKar5ucmJPDwAAA8kokUQ==";
+    "007eJxTYIia9u137pSX3/02nl4td03otfq1RvOZU1evqOJftfz1h1XvFBjMUi2Mk5NTTMzNk0xMDJOTLZMsjYxMzUzNzJISLRONko4xLUluCGRkeHvqECMjAwSC+DwMZZkpqfm6yYk5ObpGDAwA4pIm8A==";
 
 const String chatUserToken =
     "007eJxTYMjcY2i48uHZzgXpEsfmKx2auyGykqnp4s3yJ9pnFRxvyxoqMJilWhgnJ6eYmJsnmZgYJidbJlkaGZmamZqZJSVaJholcXxZlNwQyMgwJeMnAyMDKxAzMoD4KgwmBokWZiamBrrmaWbmuoaGqSm6FkmmaboGSYYWiclmicbGyWYAuhsoBw==";
